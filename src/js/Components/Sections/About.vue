68c51dd2d3b8b9c67957199fedc0e9f823b01aba
David Szabados <dv5150dev@gmail.com>
2024-07-16 00:22:08 +0200

    Updating design

--- src/js/Components/Sections/About.vue
+++ src/js/Components/Sections/About.vue
@@ -6,14 +6,14 @@ import { BriefcaseIcon, HomeIcon } from '@heroicons/vue/24/outline/index'
 <div class="flex flex-row mb-16">
   <div class="container w-1/2 pt-4">
     <p class="mont-bold">Hello Visitor,</p>
-    <p class="mont-bold text-emerald-700 text-4xl my-3">I'm David Szabados</p>
+    <p class="mont-bold text-emerald-700 text-4xl mt-2 mb-4">I'm David Szabados</p>
     <p class="flex flex-row mb-3"><BriefcaseIcon class="h-6 w-6 text-emerald-700 mr-2" /> <span class="text-emerald-700 mr-4">Laravel Developer</span><HomeIcon class="h-6 w-6 text-emerald-700 mr-2" /> <span class="text-emerald-700">Győr, Hungary</span></p>
     <p class="mont-thin text-emerald-900 text-lg">Software engineer, amateur musician, rabbit and video game enthusiast, who is building websites since 2016 and breaking guitar strings since 2009.</p>
-    <p class="text-emerald-700 my-3">I love working with <span class="mont-bold">Laravel</span>, <span class="mont-bold">Docker</span>, <span class="mont-bold">Vue</span>, <span class="mont-bold">Pinia</span> and <span class="mont-bold">Tailwind</span>.</p>
-    <p class="mont-thin text-emerald-900 text-lg">Other fancy buzzwords I'm more or less familiar with: <br> REST API, Redis, ElasticSearch, MySQL, Git, Nginx, Bootstrap</p>
+    <p class="text-emerald-700 my-3">I love working with <span class="mont-bold">Laravel</span>, <span class="mont-bold">Docker</span>, <span class="mont-bold">Vue</span>, <span class="mont-bold">Filament</span> and <span class="mont-bold">Tailwind</span>.</p>
+    <p class="mont-thin text-emerald-900 text-lg">Other fancy buzzwords I'm more or less familiar with: <br> REST API, Redis, ElasticSearch, MySQL, Git, Nginx, Pinia, Bootstrap</p>
     <p class="space-x-4 mt-4">
       <GitHubButton :url="'https://github.com/dv5150'"></GitHubButton>
-      <YouTubeButton :url="'https://youtube.com/@dv5150'"></YouTubeButton>
+      <YouTubeButton :url="'https://youtube.com/@dv5150/videos'"></YouTubeButton>
       <SpotifyButton :url="'https://spoti.fi/3YKolvJ'"></SpotifyButton>
       <LastFmButton :url="'https://www.last.fm/user/DavidFreeman777/library/artists?date_preset=ALL'"></LastFmButton>
       <FacebookButton :url="'https://www.facebook.com/dv5150fb/'"></FacebookButton>
--- src/js/Components/Sections/Gear.vue
+++ src/js/Components/Sections/Gear.vue
@@ -7,28 +7,32 @@ import { MusicalNoteIcon } from '@heroicons/vue/24/outline/index'
     <div class="container w-1/4">
       <img class="w-100 rounded-lg" src="src/img/david-szabados-2.webp" alt="David Szabados">
     </div>
-    <div class="container w-3/4 px-16 pt-4">
-      <p class="text-emerald-700 text-4xl mb-3">Music and Gear</p>
-      <p class="mont-thin text-emerald-900 text-lg mb-6">
-        I have a passion for almost all kinds of guitar music, especially for <span class="mont-base">hardcore</span>, <span class="mont-base">modern metal</span> and <span class="mont-base">punk</span> mostly in Hungarian language alongside with <span class="mont-base">Celtic rock</span>.
-        My favorite bands are <span class="mont-base">PETOFI</span>, <span class="mont-base">AWS</span>, <span class="mont-base">Carson Coma</span> and <span class="mont-base">Paddy and the Rats</span>. As a hobby I make guitar covers on YouTube, check my gear below!
+    <div class="container w-3/4 px-16">
+      <p class="mont-thin text-emerald-900 text-lg mb-8">
+        I have a passion for almost all kinds of guitar music, especially for <span class="mont-bold text-emerald-700">hardcore</span>, <span class="mont-bold text-emerald-700">modern metal</span> and <span class="mont-bold text-emerald-700">punk</span> mostly in Hungarian language alongside with <span class="mont-bold text-emerald-700">celtic rock</span>.
+        My favorite bands are <span class="mont-bold text-emerald-700">PETOFI</span>, <span class="mont-bold text-emerald-700">AWS</span>, <span class="mont-bold text-emerald-700">Carson Coma</span> and <span class="mont-bold text-emerald-700">Paddy and the Rats</span>. As a hobby I make guitar covers on <YouTubeButton :url="'https://youtube.com/@dv5150/videos'" class="align-sub p-1"></YouTubeButton>.
       </p>
-      <p class="text-emerald-700 mb-4">
-        Fender Player Telecaster <span class="mont-thin">| Butterscotch Blonde</span> <br>
-        Elixir Strings NanoWeb custom set <span class="mont-thin">| .011 .014 .018 .028 .038 .059</span> <br>
-        <span class="mont-thin">Mostly used in Drop A# and Drop B tunings</span>
+      <p class="text-emerald-700 text-lg mb-2">
+        Guitars<br>
+        <span class="mont-thin">Fender Player Telecaster <br>
+          <span class="text-sm mont-thin text-gray-500">Elixir NanoWeb custom set | .011 .014 .018 .028 .038 .059</span>
+        </span>
+        <br>
+        <span class="mont-thin">Squier Classic Vibe 50s Telecaster <br>
+          <span class="text-sm mont-thin text-gray-500">Elixir NanoWeb custom set | .009 .011 .016 .024 .032 .049</span>
+        </span>
       </p>
-      <p class="text-emerald-700 mb-4">
-        Squier Classic Vibe 50s Telecaster <span class="mont-thin">| Vintage Blonde</span> <br>
-        Elixir Strings NanoWeb custom set <span class="mont-thin">| .009 .011 .016 .024 .032 .049</span> <br>
-        <span class="mont-thin">Mostly used in Drop D tuning</span>
+      <p class="text-emerald-700 text-lg mb-2">
+        Audio Interface<br>
+        <span class="mont-thin">Focusrite Scarlett Solo 3rd Gen</span>
       </p>
-      <p class="text-emerald-700 mb-4">
-        Focusrite Scarlett Solo 3rd Gen
+      <p class="text-emerald-700 text-lg mb-2">
+        Plugins<br>
+        <span class="mont-thin">Neural Amp Modeler, ToneBoosters Equalizer 4, KiloHearts Essentials</span>
       </p>
-      <p class="text-emerald-700">
-        Neural Amp Modeler, Resington IRs, ToneBoosters Equalizer 4, KiloHearts Essentials <br>
-        Reaper, Moises, Guitar Pro 8, DaVinci Resolve
+      <p class="text-emerald-700 text-lg mb-2">
+        Software<br>
+        <span class="mont-thin">Reaper, Moises, Guitar Pro 8, DaVinci Resolve</span>
       </p>
     </div>
   </div>
